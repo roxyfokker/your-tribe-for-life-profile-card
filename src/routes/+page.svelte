8b66f31f81d98a587e63edd4333e3089e614e352
Roxy <roxyfokker@hotmail.com>
2026-09-10 21:30:55 +0200

    feat: text toegevoegd (wip) #8

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,9 @@
     import Crosses from "$lib/components/CrossPattern.svelte";
 </script>
 
+<!--
 <Crosses />
-
+-->
 <Corner class="top-right" />
 <Corner class="bottom-right" />
 <Corner class="bottom-left" />
@@ -20,8 +21,27 @@
     <p>ROLE: {roleName}</p>
 </section>
 
+<h1>Roxy Fokker</h1>
+<h2>
+    <span>Enter </span>
+    <span>The</span> 
+    <span>Profile</span>
+</h2>
+<p class="loading-text">LOADING PERSONAL DATA...</p>
+
+<p class="continue-text">PRESS ENTER TO CONTINUE_</p>
+
 
 <style>
+    :global(body){
+        height: 100vh;
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: 0;
+    }
     .system-info{ 
         position: absolute;
         top: var(--spacing-lg);
@@ -38,7 +58,6 @@
             background: transparent;
         }
     }
-
     :global(.top-right){
         position: absolute;
         top: var(--spacing-lg);
@@ -56,5 +75,51 @@
         position: absolute;
         bottom: var(--spacing-lg);
         left: var(--spacing-lg);
+    }
+
+    h1{
+        font-family: var(--font-secondary);
+        font-size: 16vw;
+        white-space: nowrap;
+        text-align: center;
+        color: color-mix(in srgb, var(--color-brand-mid) 10%, transparent);
+    }
+
+    h2{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        align-items: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: transparent;
+        span{
+            background: transparent;
+            font-family: var(--font-primary);
+            font-weight: bold;
+            font-size: 6vw;
+            text-transform: uppercase;
+            line-height: 1;
+        }
+    }
+
+    .loading-text{
+        letter-spacing: 0.4rem;
+        position: absolute;
+        font-weight: normal;
+        font-size: 1vw;
+        top: 71%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: transparent;
+    }
+    .continue-text{
+        position: absolute;
+        top: 85%;
+        left: 50%;
+        transform: translate(-50%, -50%);
     }
 </style>
